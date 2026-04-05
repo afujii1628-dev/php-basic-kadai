7e5d45178b29e4820a4ae6d055cd2a28e5ebaf95
afujii1628-dev <a.fujii1628@gmail.com>
2026-04-05 14:13:30 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,7 @@
+<?php
+$vegetable_date = ['名前' => '玉ねぎ', '値段' => '200', '産地' => '北海道'];
+
+// 配列$vegetable_dateのキーと値を1つずつ順番に出力する
+foreach ($vegetable_date as $key => $value) {
+  echo "{$key}:{$value} <br>";
+}
